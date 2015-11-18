e94735805f01ae509eef9f095f62a4af679f1e2c
sauy7 <tim@heighes.com>
2015-11-18 12:13:50 +0100

    fixed syntax

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name 'libgmp3-dev'
-description 'Cookbook to install libgmp3-dev on Ubuntu'
-maintainer: 'Tim Heighes'
-maintainer_email: 'tim@onegiantleap.se'
-license: 'The unlicense'
-version '0.0.1'
-supports 'ubuntu', '>= 14.04'
+name             'libgmp3-dev'
+description      'Cookbook to install libgmp3-dev on Ubuntu'
+maintainer       'Tim Heighes'
+maintainer_email 'tim@onegiantleap.se'
+license          'The unlicense'
+version          '1.0.0'
+supports         'ubuntu', '>= 14.04'
 
-recipe "default", "Install libgmp3 development defaults package"
+recipe           'default', 'Install libgmp3 development defaults package'
